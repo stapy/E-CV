56f38f995700e622281d17179f02db99f2c35004
Stapy <stapyb@gmail.com>
2015-04-12 17:30:13 +0200

    Create CvIndex.php
    This is the original index page we made a mistake

---
+++ CvIndex.php
@@ -0,0 +1,110 @@
+<html>
+	<head>
+
+		<title>Cv-Creator</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet"  href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+
+	</head>
+		<body>
+			<div class="navbar navbar-inverse navbar-static-top">
+				<div class="container">
+					<a href="index.html" class="navbar-brand">Cv Creator</a>
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+					<div class="collapse navbar-collapse navHeaderCollapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="Cv.html">CV</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Social Media</a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Facebook</a></li>
+								<li><a href="#">Google+</a></li>
+
+							</ul>
+							</li>
+						</ul>
+					</div>	
+
+				</div>
+
+			</div>
+
+
+			
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+
+
+
+			<div class="container">
+				<div class="jumbotron">
+					<center><h1>Welcome to Cv Creator!</h1>
+					<p>We hope you will enjoy this web-application and find it usefull</p>
+					<a href="Cv.html" class="btn btn-primary btn-large">Create your CV!</a></center>
+
+				</div>
+
+
+			</div>
+
+			
+			<br/>
+
+			<div class="container">
+				
+						<h1><a href="#">Cv Creator goal & Mission</a></h1>
+						<p>CV Creator's mission and goal is to help people to create thier cv in a much and faster way.So basically what CV Creator does is that it lays out the fornate for you and all you just do is to fill in the forms and after finishing you can than print out the cv or you could save it on a txt format for later use . </p>
+
+
+
+						<h3>Created By Stavios</h3>
+
+
+					</div>
+
+
+				
+
+
+			</div>
+			
+
+
+
+			<div class="navbar navbar-default navbar-fixed-bottom">
+
+				<div class="container">
+
+					<p class="navbar-text pull-left">Create your CV the easy way</p>
+					<a href="#" class="navbar-btn btn-danger btn pull-right">Subscribe on youtube</a>
+
+				</div>
+
+
+
+			</div>
+
+
+
+			<script src="jquery.js"></script>
+			<script src="js/bootstrap.js"></script>
+		</body>
+
+</html>
